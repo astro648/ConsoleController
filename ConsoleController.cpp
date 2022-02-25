cd5af688db200212740d79094a80c608d823cc60
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-02-25 10:31:26 -0800

    Update ConsoleController.cpp

--- ConsoleController.cpp
+++ ConsoleController.cpp
@@ -76,10 +76,18 @@ int main()
 			}
 			else
 			{
-				cout << "The application name abbreviation you entered was not recognized. If you would like to, you can try again." << endl;
+				cout << "The application name abbreviation command you entered was not recognized. If you would like to, you can try again." << endl;
 			}
 		}
 		else if (input1 == "copyright")
+		{
+			cout << "ConsoleController - a program which helps you do many things in one place. See GitHub repo for more info. Copyright (C) 2022 Ayaan R | This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>." << endl;
+		}
+		else if (input1 == "copyright-info")
+		{
+			cout << "ConsoleController - a program which helps you do many things in one place. See GitHub repo for more info. Copyright (C) 2022 Ayaan R | This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>." << endl;
+		}
+		else if (input1 == "c")
 		{
 			cout << "ConsoleController - a program which helps you do many things in one place. See GitHub repo for more info. Copyright (C) 2022 Ayaan R | This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>." << endl;
 		}
